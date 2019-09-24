5ad8d89401516e63ba028540706710b4ffa9c054
Neil Simmons <neil@ruckit.me>
2019-09-24 16:06:49 -0500

    CORS support and schema matching to sprout

--- handler.py
+++ handler.py
@@ -18,10 +18,17 @@ def get_vision_client():
 def ocr_image(image_url):
     client = get_vision_client()
     res = client.document_text_detection({'source': {'image_uri': image_url}}, timeout=5.0)
-    data = MessageToDict(res).get('textAnnotations', None)
+    ocr_result = MessageToDict(res)
+    text_annotations = ocr_result.get('textAnnotations', [])
+    ocr_result_text = [item.get('description', '') for item in text_annotations]
     response = {
-        "statusCode": 200,
-        "body": json.dumps(data)
+        "attachment": image_url,
+        "ocr_result": {
+            "responses": [
+                ocr_result
+            ]
+        },
+        "ocr_result_text": ocr_result_text
     }
     return response
 
@@ -29,9 +36,36 @@ def ocr_image(image_url):
 def lambda_handler(event, context):
     """AWS Lambda Handler for API Gateway input"""
     post_args = json.loads(event.get("body", {}))
-    image_url = post_args["image_url"]
+    ticket = post_args["ticket"]
+    ticket_id = ticket["identifier"]
+    image_url = ticket["remote_attachment_url"]
 
     logging.debug("Detecting image from URL: %s" % image_url)
 
-    response = ocr_image(image_url)
+    response_body = {
+        "id": None,
+        "identifier": ticket_id,
+        "attachment": image_url,
+        "attachment_height": None,
+        "attachment_width": None,
+        "created_at": None,
+        "driver": None,
+        "errors": None,
+        "properties": None,
+        "status": None,
+        "tags": None,
+        "text_matches": None,
+        "truck": None,
+        "updated_at": None
+    }
+    response_body.update(ocr_image(image_url))
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": True,
+        },
+        "body": json.dumps(response_body)
+    }
+
     return response
